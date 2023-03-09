c395f52313275a5349838e646468b2d2c7d55a9f
LeandroRapan <leandro.rapan@gmail.com>
2023-03-09 20:29:42 -0300

    PreEntrega1 Rapan

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,5 +1,6 @@
 import cartlogo from "./assets/online-shop.png"
 const CartWidget = () => {
+    // estilos y numero estatico
     return (
         <div className="nav-item " style={{display:"flex", justifyContent:"space-around", alignItems:"center", width:"30px"}}>
             <img src={cartlogo} alt="cart widget" className="img-fluid" />
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import CartWidget from "../CartWidget/CartWidget"
 import "./NavBar.css"
 
 // NavBar con estilos de bootstrap y estilos css en linea
+// no me gusta mucho los estilos en linea  Hice un NavBar.css por que bootstrap se me esta resistiendo, pero tener parte en un archivo y parte en linea me parece desprolijo. Lo añadi al git ignore y lo voy a seguir trabajando por separados
 const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-xl  " style={{backgroundColor: "#227C70"}} >
